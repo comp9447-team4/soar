e8e5a46f8a3108896053a9abc06277da9fc95396
jtdv01 <jtdv01@gmail.com>
2020-09-18 19:20:38 +1000

    wip

--- bin/user_admin.sh
+++ bin/user_admin.sh
@@ -5,20 +5,32 @@
 set -eu
 
 export REPO_ROOT=$(git rev-parse --show-toplevel)
-export USER_ROLE_STACK_NAME="user-roles"
+export USER_ROLES_STACK_NAME="user-roles"
+export USERS_STACK_NAME="users"
 
 create_user_role_stack() {
     echo "Creating..."
-    aws cloudformation create-stack --stack-name "${USER_ROLE_STACK_NAME}" \
+    aws cloudformation create-stack --stack-name "${USER_ROLES_STACK_NAME}" \
         --template-body file://"${REPO_ROOT}"/users/roles.yml \
         --parameters file://"${REPO_ROOT}"/users/users-parameters.json \
         --capabilities CAPABILITY_NAMED_IAM
+}
 
+create_users_stack() {
+    echo "Creating..."
+    aws cloudformation create-stack --stack-name "${USERS_STACK_NAME}" \
+        --template-body file://"${REPO_ROOT}"/users/users.yml \
+        --capabilities CAPABILITY_NAMED_IAM
+}
+
+delete_users_stack() {
+    echo "Deleting..."
+    aws cloudformation delete-stack --stack-name "${USERS_STACK_NAME}"
 }
 
 delete_user_role_stack() {
     echo "Deleting..."
-    aws cloudformation delete-stack --stack-name "${USER_ROLE_STACK_NAME}"
+    aws cloudformation delete-stack --stack-name "${USER_ROLES_STACK_NAME}"
 }
 
 main() {
@@ -27,8 +39,12 @@ main() {
 
     if [ "${args}" == "delete-user-role-stack" ]; then
         delete_user_role_stack
-    elif [ "${args}" == "deploy-user-role-stack" ]; then
+    elif [ "${args}" == "create-user-role-stack" ]; then
         create_user_role_stack
+    elif [ "${args}" == "create-users-stack" ]; then
+        create_users_stack
+    elif [ "${args}" == "delete-users-stack" ]; then
+        delete_users_stack
     fi
 
 }
