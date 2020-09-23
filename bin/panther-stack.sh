2246ee8d3116141123dcc3fae2507d15c4766558
jtdv01 <jtdv01@gmail.com>
2020-09-23 07:40:11 -0400

    deploy panthers (#12)
    * Added panthers for us-east-1
    
    * Added Panther stack in us-east-1
    
    * docs

---
+++ bin/panther-stack.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Deploys Panther stack. Must be in us-east-1.
+# https://docs.runpanther.io/quick-start
+
+source "${REPO_ROOT}"/bin/_utils.sh
+
+create() {
+    AWS_REGION=us-east-1 aws cloudformation deploy \
+        --template-file "${REPO_ROOT}"/infra/panther/panther.yml \
+        --stack-name panther \
+        --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+}
+
+main() {
+    args="$@"
+    check_environment
+
+    if [ "${args}" == "create" ]; then
+        create
+    elif [ "${args}" == "delete" ]; then
+        delete
+    elif [ "${args}" == "update" ]; then
+        update
+    elif [ "${args}" == "describe" ]; then
+        describe
+    else
+        usage
+    fi
+
+}
+
+main "$@"
