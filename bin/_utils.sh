1d3a682e3d861dd2f23674e8e4d22d7f547ef674
jtdv01 <jtdv01@gmail.com>
2020-09-23 06:49:00 -0400

    Added budgets stack (#11)

--- bin/_utils.sh
+++ bin/_utils.sh
@@ -6,12 +6,16 @@ set -eu
 export REPO_ROOT=$(git rev-parse --show-toplevel)
 
 check_environment() {
+    echo "Checking if you have set AWS_PROFILE..."
+    echo "If this step fails, make sure you have setup your AWS CLI properly. See README."
     if [ "${AWS_PROFILE}" == "qa" ]; then
         echo "In environment: QA"
     elif [ "${AWS_PROFILE}" == "prod" ]; then
         echo "In environment: prod"
     elif [ "${AWS_PROFILE}" == "master-admin" ]; then
         echo "In environment: master-admin"
+    elif [ "${AWS_PROFILE}" == "prod-admin" ]; then
+        echo "In environment: prod-admin"
     else
         echo "Unknown AWS_PROFILE. Must be 'qa' or 'prod'. Did you setup your aws cli properly? See README."
         echo "Exiting..."
