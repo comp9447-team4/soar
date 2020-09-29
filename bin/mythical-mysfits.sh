fb6a25341063de59b34e22b647f2d25d6a34c408
jtdv01 <jtdv01@gmail.com>
2020-09-30 01:04:58 +1000

    Added ecr

--- bin/mythical-mysfits.sh
+++ bin/mythical-mysfits.sh
@@ -5,29 +5,32 @@ set -u
 
 source "${REPO_ROOT}"/bin/_utils.sh
 
-export MODULE_1_STACK_NAME="MythicalMystfitsStaticSiteStack"
-export MODULE_1_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/static-site.yml"
-
-export MODULE_2_CORE_STACK_NAME="MythicalMysfitsCoreStack"
-export MODULE_2_CORE_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/core.yml"
-
 export AWS_REGION="us-east-1"
 
+# Module 1
+export STATIC_SITE_STACK_NAME="MythicalMystfitsStaticSiteStack"
+export STATIC_SITE_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/static-site.yml"
+# Module 2
+export CORE_STACK_NAME="MythicalMysfitsCoreStack"
+export CORE_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/core.yml"
+export ECR_STACK_NAME="MythicalMysfitsECRStack"
+export ECR_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/ecr.yml"
 
+# Module 1
 create_static_site() {
     local bucket_name
     bucket_name="${AWS_PROFILE}-comp9447-team4-mythical-mysfits"
 
     echo "Deploying bucket stack..."
     aws cloudformation create-stack \
-        --stack-name "${MODULE_1_STACK_NAME}" \
-        --template-body file://"${MODULE_1_STACK_YML}" \
+        --stack-name "${STATIC_SITE_STACK_NAME}" \
+        --template-body file://"${STATIC_SITE_STACK_YML}" \
         --parameters ParameterKey=BucketName,ParameterValue="${bucket_name}" \
         --enable-termination-protection
 
     wait
-    echo "Waiting for stack to be created... (ugh not the best way to do this...)"
-    sleep 10
+    echo "Waiting for stack to be created..."
+    wait_build "${STATIC_SITE_STACK_NAME}"
     echo "Copying to S3..."
     aws s3 cp \
         "${REPO_ROOT}"/mythical-mysfits/web/index.html \
@@ -39,13 +42,32 @@ create_static_site() {
     curl "${url}" | head -15
 }
 
+# Module 2
 create_core() {
     # https://github.com/aws-samples/aws-modern-application-workshop/tree/python/module-2
     aws cloudformation create-stack \
-        --stack-name "${MODULE_2_CORE_STACK_NAME}" \
-        --template-body file://"${MODULE_2_CORE_STACK_YML}" \
+        --stack-name "${CORE_STACK_NAME}" \
+        --template-body file://"${CORE_STACK_YML}" \
         --capabilities CAPABILITY_NAMED_IAM \
         --enable-termination-protection
+}
+
+create_ecr() {
+    aws cloudformation create-stack \
+        --stack-name "${ECR_STACK_NAME}" \
+        --template-body file://"${ECR_STACK_YML}" \
+        --capabilities CAPABILITY_NAMED_IAM \
+        --enable-termination-protection
+
+}
+
+build_docker_image() {
+    account_id=$(aws sts get-caller-identity | jq -r '.Account')
+    cd "${REPO_ROOT}/mythical-mysfits/Dockerfile"
+    docker build \
+           . \
+           -t "${account_id}.dkr.ecr.${AWS_REGION}.amazonaws.com/mythicalmysfits/service:latest"
+    cd -
 }
 
 usage() {
@@ -77,7 +99,12 @@ main() {
         # Module 1
         create_static_site
     elif [[ "${args}" == "create-module-2" ]]; then
-        create_core
+        wait_build "${STATIC_SITE_STACK_NAME}"
+        # create_core
+        wait_build "${CORE_STACK_NAME}"
+        create_ecr
+        wait_build "${ECR_STACK_NAME}"
+        build_docker_image
     else
         echo "No command run :("
         usage
