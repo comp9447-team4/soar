5fe172920f4f0a95abe55d9e57f58f7f10e112c7
jtdv01 <jtdv01@gmail.com>
2020-09-30 23:10:20 +1000

    Damn fargaatteee,, id rather k8s

--- bin/_utils.sh
+++ bin/_utils.sh
@@ -29,3 +29,12 @@ wait_build() {
     aws cloudformation wait stack-exists --stack-name "${stack_name}" > /dev/null
     aws cloudformation wait stack-create-complete --stack-name "${stack_name}" > /dev/null
 }
+
+get_cfn_export() {
+    local export_names=$(aws cloudformation list-exports | jq -r '.Exports' )
+    local val=$(echo "${export_names}" |
+                       jq -c "map(select(.Name==\"$1\")| .Value)" |
+                       jq -r '.[0]' |
+                       sed 's/\//\\\//g')
+    echo "$val"
+}
--- bin/mythical-mysfits.sh
+++ bin/mythical-mysfits.sh
@@ -12,6 +12,8 @@ export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 # Module 1
 export STATIC_SITE_STACK_NAME="MythicalMystfitsStaticSiteStack"
 export STATIC_SITE_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/static-site.yml"
+export STATIC_SITE_BUCKET_NAME="${AWS_PROFILE}-comp9447-team4-mythical-mysfits"
+
 # Module 2
 export CORE_STACK_NAME="MythicalMysfitsCoreStack"
 export CORE_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/core.yml"
@@ -19,22 +21,20 @@ export ECR_STACK_NAME="MythicalMysfitsECRStack"
 export ECR_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/ecr.yml"
 export ECS_STACK_NAME="MythicalMysfitsECSStack"
 export ECS_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/ecs.yml"
-export FARGATE_SERVICE_STACK_NAME="MythicalMysfitsECSStack"
-export FARGATE_SERVICE_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/ecs.yml"
+export FARGATE_SERVICE_STACK_NAME="MythicalMysfitsFargateServiceStack"
+export FARGATE_SERVICE_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/fargate-service.yml"
 export CICD_STACK_NAME="MythicalMysfitsCICDStack"
 export CICD_STACK_YML="${REPO_ROOT}/mythical-mysfits/cfn/cicd.yml"
 export MYTHICAL_MYSFITS_REPO="${REPO_ROOT}/../MythicalMysfitsService-Repository"
 
 # Module 1
 create_static_site() {
-    local bucket_name
-    bucket_name="${AWS_PROFILE}-comp9447-team4-mythical-mysfits"
 
     echo "Deploying bucket stack..."
     aws cloudformation create-stack \
         --stack-name "${STATIC_SITE_STACK_NAME}" \
         --template-body file://"${STATIC_SITE_STACK_YML}" \
-        --parameters ParameterKey=BucketName,ParameterValue="${bucket_name}" \
+        --parameters ParameterKey=BucketName,ParameterValue="${STATIC_SITE_BUCKET_NAME}" \
         --enable-termination-protection
 
     wait
@@ -105,17 +105,37 @@ create_ecs() {
         --enable-termination-protection
     wait_build "${ECS_STACK_NAME}"
 
+}
+
+create_fargate_service(){
     echo "Creating fargate service separately because cfn doesn't play well with it..."
     echo "https://stackoverflow.com/questions/32727520/cloudformation-template-for-creating-ecs-service-stuck-in-create-in-progress"
 
-    local task_def_arn=$(aws ecs list-task-definitions --family-prefix mythicalmysfits | jq -r .taskDefinitionArns[0][-1:])
-    aws cloudformation create-stack \
-        --stack-name "${FARGATE_SERVICE_STACK_NAME}" \
-        --template-body file://"${FARGATE_SERVICE_STACK_YML}" \
-        --capabilities CAPABILITY_NAMED_IAM \
-        --parameters ParameterKey=TaskDefArn,ParameterValue="${task_def_arn}" \
-        --enable-termination-protection
-    wait_build "${FARGATE_SERVICE_STACK_NAME}"
+
+    local nlb_tg=$(get_cfn_export MythicalMysfitsECSStack:NLBTargetGroup)
+    local sg=$(get_cfn_export MythicalMysfitsCoreStack:FargateContainerSecurityGroup)
+    local subnet_one=$(get_cfn_export MythicalMysfitsCoreStack:PrivateSubnetOne)
+    local subnet_two=$(get_cfn_export MythicalMysfitsCoreStack:PrivateSubnetTwo)
+
+    local parameters
+    parameters=$(cat "${REPO_ROOT}"/mythical-mysfits/module-2/aws-cli/service-definition.json |
+                     sed "s/REPLACE_ME_SECURITY_GROUP_ID/${sg}/g" |
+                     sed "s/REPLACE_ME_NLB_TARGET_GROUP_ARN/${nlb_tg}/g" |
+                     sed "s/REPLACE_ME_PRIVATE_SUBNET_ONE/${subnet_one}/g" |
+                     sed "s/REPLACE_ME_PRIVATE_SUBNET_TWO/${subnet_two}/g"
+    )
+
+    echo "${parameters}"
+    #aws ecs create-service \
+    #    --cli-input-json "${parameters}"
+
+    # local task_def_arn=$(aws ecs list-task-definitions --family-prefix mythicalmysfitsservice | jq -r '.taskDefinitionArns[0]')
+    # aws cloudformation create-stack \
+    #     --stack-name "${FARGATE_SERVICE_STACK_NAME}" \
+    #     --template-body file://"${FARGATE_SERVICE_STACK_YML}" \
+    #     --capabilities CAPABILITY_NAMED_IAM \
+    #     --parameters ParameterKey=TaskDefArn,ParameterValue="${task_def_arn}" \
+    #     --enable-termination-protection
 }
 
 create_cicd() {
@@ -128,6 +148,10 @@ create_cicd() {
         --parameters ParameterKey=BucketName,ParameterValue="${bucket_name}" \
         --enable-termination-protection
     wait_build "${CICD_STACK_NAME}"
+}
+
+update_bucket() {
+    aws s3 cp "${REPO_ROOT}"/mythical-mysfits/module-2/web/index.html s3://"${STATIC_SITE_BUCKET_NAME}"/index.html
 }
 
 init_mystical_mysfits_repo() {
@@ -177,12 +201,16 @@ main() {
     elif [[ "${args}" == "create-module-2" ]]; then
         # create_core
         # create_ecr
+
         # build_docker_image
         # push_image_to_ecr
 
-        create_ecs
+        # create_ecs
+        # update_bucket
 
-        create_cicd
+        create_fargate_service
+        # create_cicd
+        # init_mystical_mysfits_repo
 
     else
         echo "No command run :("
