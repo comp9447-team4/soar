50bc3ad6963abd9a1a6356192b609f0f7b8aaacc
jtdv01 <jtdv01@gmail.com>
2020-10-07 00:51:09 +1100

    Enabled xray

--- mythical-mysfits/questions-service/ProcessQuestionsStream/mysfitsProcessStream.py
+++ mythical-mysfits/questions-service/ProcessQuestionsStream/mysfitsProcessStream.py
@@ -2,13 +2,13 @@ import boto3
 import json
 import os
 # UNCOMMENT_BEFORE_2ND_DEPLOYMENT
-#from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import xray_recorder
 
 # UNCOMMENT_BEFORE_2ND_DEPLOYMENT
-#from aws_xray_sdk.core import patch_all
+from aws_xray_sdk.core import patch_all
 
 # UNCOMMENT_BEFORE_2ND_DEPLOYMENT
-#patch_all()
+patch_all()
 
 snsTopic = os.environ['SNS_TOPIC_ARN']
 sns = boto3.client('sns')
