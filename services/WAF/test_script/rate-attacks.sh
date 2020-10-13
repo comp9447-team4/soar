d91a1162c035f2fd4f1929039b1baaf95dd4c8c6
Chirag <txtchirag@yahoo.com>
2020-10-13 17:58:37 +1100

    Update rate-attacks.sh

--- services/WAF/test_script/rate-attacks.sh
+++ services/WAF/test_script/rate-attacks.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 token="${TOKEN}"
+url="${URL}"
 for (( ; ; )); do \
-curl 'https://6jztqidvd6.execute-api.us-east-1.amazonaws.com/qa/mysfits/33e1fbd4-2fd8-45fb-a42f-f92551694506/like' \
+curl "${url}" \
   -X 'POST' \
   -H "authorization: ${token}" \
   --compressed; \
