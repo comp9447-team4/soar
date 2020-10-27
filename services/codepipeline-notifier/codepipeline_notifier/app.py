40cbde7c0c34fecf61b8575313480a40009f8c0e
jtdv01 <jtdv01@gmail.com>
2020-10-27 23:48:15 +1100

    jt codebuild notifications (#77)
    * Codepipeline notifier
    
    * Created notification rule
    
    * 2
    
    * now too many
    
    * notifier
    
    * reduce noise
    
    * app
    
    * Clean up
    
    * updated cicd of soar

---
+++ services/codepipeline-notifier/codepipeline_notifier/app.py
@@ -0,0 +1,42 @@
+import json
+import logging
+import os
+import requests
+
+# Setup logging
+# https://docs.aws.amazon.com/lambda/latest/dg/python-logging.html#python-logging-lib
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    """
+
+    AWS_ENVIRONMENT = os.environ["AWS_ENVIRONMENT"]
+
+    webhook_url = ""
+    if AWS_ENVIRONMENT == "qa":
+        webhook_url = os.environ["DISCORD_DEV_ALERTS_CHANNEL_WEBHOOK"]
+    elif AWS_ENVIRONMENT == "prod":
+        webhook_url = os.environ["DISCORD_ALERTS_CHANNEL_WEBHOOK"]
+    else:
+        raise ValueError(f"Unknown AWS_ENVIRONMENT: {AWS_ENVIRONMENT}. Must be qa or prod.")
+
+    message = event['Records'][0]['Sns']['Message']
+    logger.info(f"Got a message: {message}")
+
+    content = f"I am the CodePipeline bot. :robot:\n```{message}```"
+    response = requests.post(webhook_url, data={"content": content})
+    return 0
