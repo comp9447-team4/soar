72d15f6cbea2bb32c7db98872666fbcfaecc9a32
stevlian <stevlian@cisco.com>
2020-11-10 02:03:29 +1100

    got prompts working

--- cli-tool/lib/tools/SOAR_PARSER.py
+++ cli-tool/lib/tools/SOAR_PARSER.py
@@ -1,7 +1,38 @@
+from __future__ import print_function, unicode_literals
 import sys
 import click 
 from os import path
+from PyInquirer import style_from_dict, Token, prompt, Separator
+from pprint import pprint
 import boto3
+
+
+style = style_from_dict({
+    Token.Separator: '#cc5454',
+    Token.QuestionMark: '#673ab7 bold',
+    Token.Selected: '#cc5454',  # default
+    Token.Pointer: '#673ab7 bold',
+    Token.Instruction: '',  # default
+    Token.Answer: '#f44336 bold',
+    Token.Question: '',
+})
+
+def build_prompts(q_message, q_name, options):
+    #create a prompt for the user to select something
+    convert_options = [{'name': i} for i in options]
+    q = [
+        {
+            'type': 'checkbox',
+            'message': q_message,
+            'name': q_name,
+            'choices': convert_options,
+            'validate': lambda answer: 'You must choose at one option' \
+                if len(answer) == 0 else True
+        }
+    ]
+    
+    return q
+
 
 #Parses class which parses the user import and deploy AWS security stack. 
 class SOAR_PARSER():
@@ -10,14 +41,19 @@ class SOAR_PARSER():
         self.template_folder = args['template_folder']
         self.inventory_folder = args['inv_folder']
         self.mapping_yml = args['mapping_cfg']
+        
     
     def scan_serveless(self):
         print(f"Scanning for serveless applicaiton in account ...")
         s3_session = self.aws_session.client('s3')
         #list of buckets
         s3_buckets = [i['Name'] for i in s3_session.list_buckets()['Buckets']]
+        #print(s3_buckets)
+        s3_prompts = build_prompts("Deploy stack on the following s3 buckets:", "s3 buckets", s3_buckets)
+        user_input = prompt(s3_prompts, style=style)
 
-        print(s3_buckets)
+        pprint(user_input)
+
     #generate a play to be executed after parsing user input
     def execute_play(self):
         pass
