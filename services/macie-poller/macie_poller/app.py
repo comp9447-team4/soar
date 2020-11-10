b961198e0a978a30458c00f53ea8e50ea9948891
Evangeline Endacott <z5209033@student.unsw.edu.au>
2020-11-10 20:36:29 +1100

    still need to work on the lambda but am unable to run the build to test output

--- services/macie-poller/macie_poller/app.py
+++ services/macie-poller/macie_poller/app.py
@@ -6,18 +6,23 @@ import requests
 
 def lambda_handler(event, context):
     """
-    What does this lambda do?
+    Send an alert of the AWS Macie findings to Discord
     """
 
-    # client = boto3.client('macie')
-    # finding_ids = client.list_findings()
-    # for i in finding_ids:
-    # some_json = client.get_findings(id)
+    client = boto3.client('macie2')
+    finding_ids = client.list_findings()
+    for finding in finding_ids.findingsIds:
+        macie_findings = client.get_findings(finding)
+        
 
-
+    is_dev=os.envrion["IS_DEV"]
     if is_dev == "1":
         webhook_url = os.environ["DISCORD_DEV_ALERTS_CHANNEL_WEBHOOK"]
     else:
         webhook_url = os.environ["DISCORD_ALERTS_CHANNEL_WEBHOOK"]
 
-    # return requests.post(webhook_url, content)
+    # Below is the response that will be sent to Discord
+    content="""<insert the content here"""
+
+    response = requests.post(webhook_url, data={"content": content})
+    return response
