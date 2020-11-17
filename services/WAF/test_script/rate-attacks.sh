245ed712841f35b18b339dc960c437daf236fe60
chirag <txtchirag@yahoo.com>
2020-11-17 16:25:43 +1100

    waf prod

--- services/WAF/test_script/rate-attacks.sh
+++ services/WAF/test_script/rate-attacks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for (( ; ; )); do \
-curl 'https://6jztqidvd6.execute-api.us-east-1.amazonaws.com/qa/mysfits/b6d16e02-6aeb-413c-b457-321151bb403d' \
+curl 'https://6jztqidvd6.execute-api.us-east-1.amazonaws.com/qa/mysfits/c0684344-1eb7-40e7-b334-06d25ac9268c' \
   --compressed; \
 done
