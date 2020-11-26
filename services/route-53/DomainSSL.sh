da3a3373bf1bf4e0391a0510d4099eda1d5e7ca0
Steve <s.liang@student.unsw.edu.au>
2020-11-26 13:02:08 +1100

    Update DomainSSL.sh

--- services/route-53/DomainSSL.sh
+++ services/route-53/DomainSSL.sh
@@ -1,16 +1,16 @@
-#hardcode the local dev account for test
+#Variables
 export AWS_ENVIRONMENT="qa"
 export AWS_DEFAULT_REGION=us-east-1
 export Route53_NAME="${AWS_ENVIRONMENT}-53-SSL-Stack"
 export Route_Config_YML="./ssl.yml"
-
+export My_domain="Insert your domain"
 
 echo "Creating route 53 host zone..."
 aws cloudformation create-stack \
     --stack-name "${Route53_NAME}" \
-    --region us-east-1 \
+    --region "${AWS_DEFAULT_REGION}" \
     --template-body file://"${Route_Config_YML}" \
     --capabilities CAPABILITY_NAMED_IAM \
-    --parameters ParameterKey="DomainName",ParameterValue="9447.me"\
+    --parameters ParameterKey="DomainName",ParameterValue="${My_domain}"\
      ParameterKey="HostedZoneId",ParameterValue="Z0356916NZWKOARDXAL1"
 
